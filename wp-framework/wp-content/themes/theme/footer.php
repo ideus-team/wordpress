51822898f3a27c73e06d002286460f534d75a375
Valery Bogdanov <radkiller@yandex.ru>
2014-10-31 13:42:59 +0200

    Update wp-framework

--- wp-framework/wp-content/themes/theme/footer.php
+++ wp-framework/wp-content/themes/theme/footer.php
@@ -16,7 +16,7 @@
       </div>
 
       <address class="l-siteCopyright vcard" itemscope itemtype="http://schema.org/Organization">
-        <div class="b-siteCopyright">© <?php echo date('Y'); ?> <a rel="me" itemprop="name" class="b-siteCopyright__link fn n org url work" href="<?php echo site_url('/'); ?>">%clientName%</a>. Все права защищены</div>
+        <div class="b-siteCopyright">© <?php echo date('Y'); ?> <a rel="me" itemprop="name" class="b-siteCopyright__link fn n org url work" href="<?php echo site_url('/'); ?>"><?php bloginfo('name'); ?></a>. Все права защищены</div>
         <div class="b-developerCopyright">Разработано в <a rel="friend" class="b-developerCopyright__link" href="http://ideus.biz/" target="_blank">iDeus</a></div>
       </address>
     </footer>
