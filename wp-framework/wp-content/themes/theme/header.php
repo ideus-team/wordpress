04553c727625b46b7da0cb8cda195aecefb0b82d
Valery Bogdanov <radkiller@yandex.ru>
2014-10-31 14:40:37 +0200

    Add searchform

--- wp-framework/wp-content/themes/theme/header.php
+++ wp-framework/wp-content/themes/theme/header.php
@@ -59,13 +59,7 @@ switch (nc_device()) {
         <?php endif; ?>
       </div>
 
-      <div class="l-siteSearch">
-        <form class="b-siteSearch" role="search" method="post" action="#">
-          <label class="b-siteSearch__label g-visuallyhidden" for="search-text">Найти:</label>
-          <input class="b-siteSearch__input" type="search" spellcheck="true" value="" name="search" id="search-text" />
-          <input class="b-siteSearch__button" type="submit" value="Search" />
-        </form>
-      </div>
+      <?php get_search_form(); ?>
 
       <nav class="l-siteNavigation" role="navigation">
         <?php
---
+++ wp-framework/wp-content/themes/theme/searchform.php
@@ -0,0 +1,7 @@
+<div class="l-siteSearch">
+  <form class="b-siteSearch" role="search" action="<?php echo site_url('/'); ?>" method="get">
+    <label class="b-siteSearch__label g-visuallyhidden" for="search-text">Найти:</label>
+    <input class="b-siteSearch__input" type="search" type="search" name="s" value="" spellcheck="true" />
+    <input class="b-siteSearch__button" type="submit" value="Search" />
+  </form>
+</div>
