2a196e5bfd9c21e293bc376a0a57b57da3b1cb18
Valery Bogdanov <radkiller@yandex.ru>
2014-10-31 22:00:58 +0200

    update index.php

--- wp-framework/wp-content/themes/theme/index.php
+++ wp-framework/wp-content/themes/theme/index.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
 <main class="l-contentText" role="main">
-
+  <?php while (have_posts()) : the_post(); ?>
+    <section class="b-contentText b-text">
+      <?php the_content(); ?>
+    </section>
+  <?php endwhile; ?>
 </main>
 
 <?php get_footer(); ?>
