9885c3f9fe5051e7db7671e4a40217efd7b8200c
Valery Bogdanov <valery.bogdanov@ideus.biz>
2014-12-24 14:27:48 +0200

    Add modify excerpt

--- wp-framework/wp-content/themes/theme/functions.php
+++ wp-framework/wp-content/themes/theme/functions.php
@@ -35,7 +35,7 @@ function nc_setup() {
 add_action('wp_enqueue_scripts', 'nc_scripts');
 function nc_scripts() {
   wp_deregister_script('jquery');
-  if (iever('<=', 8)) {
+  if (nc_iever('<=', 8)) {
     wp_register_script('jquery', get_template_directory_uri().'/js/vendor/jquery-1.11.1.min.js', false, '1.11.1');
     wp_register_script('ie8', get_template_directory_uri().'/js/legacy/ie8.js', false, null);
   } else {
@@ -47,7 +47,7 @@ function nc_scripts() {
 
 
 // Detects which version of Internet Explorer browser the user is using.
-function iever($compare=false, $to=NULL){
+function nc_iever($compare=false, $to=NULL){
   if (!preg_match('/MSIE (.*?);/', $_SERVER['HTTP_USER_AGENT'], $m) || preg_match('#Opera#', $_SERVER['HTTP_USER_AGENT'])) {
     return false === $compare ? false : NULL;
   }
@@ -56,5 +56,17 @@ function iever($compare=false, $to=NULL){
   } else {
     return (int)$m[1];
   }
+}
+
+
+// Modify excerpt
+add_filter('wp_trim_excerpt', 'nc_excerpt_more');
+function nc_excerpt_more($excerpt) {
+  return str_replace('[&hellip;]', '…', $excerpt);
+}
+
+add_filter('excerpt_length', 'nc_excerpt_length');
+function nc_excerpt_length($length) {
+	return 20;
 }
 ?>
