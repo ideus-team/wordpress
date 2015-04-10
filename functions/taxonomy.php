b087044dae5de4065a835c1f81defec39e3a534f
RAD <radkiller@yandex.ru>
2015-04-10 03:53:40 +0300

    Update register_taxonomy

--- functions/taxonomy.php
+++ functions/taxonomy.php
@@ -1,30 +1,31 @@
 <?php
-//Регистрируем новую таксономию для раздела «Вопросы и ответы»
-add_action('init', 'nc_taxonomy_faq', 0);
-function nc_taxonomy_faq() {
+// New taxonomy for Blog
+add_action('init', 'nc_taxonomy_blog', 0);
+function nc_taxonomy_blog() {
   $labels = array(
-    'name'              => 'Категории',
-    'singular_name'     => 'Категория',
-    'search_items'      => 'Найти категорию',
-    'all_items'         => 'Все категории',
-    'parent_item'       => 'Родительская категория',
-    'parent_item_colon' => 'Родительская категория:',
-    'edit_item'         => 'Редактировать категорию',
-    'view_item'         => 'Просмотреть категорию',
-    'update_item'       => 'Обновить категорию',
-    'add_new_item'      => 'Добавить новую категорию',
-    'new_item_name'     => 'Название',
-    'menu_name'         => 'Категории'
+    'name'              => 'Blog Categories',
+    'singular_name'     => 'Category',
+    'search_items'      => 'Search Categories',
+    'all_items'         => 'All Categories',
+    'parent_item'       => 'Parent Category',
+    'parent_item_colon' => 'Parent Category:',
+    'edit_item'         => 'Edit Category',
+    'view_item'         => 'View Category',
+    'update_item'       => 'Update Category',
+    'add_new_item'      => 'Add New Category',
+    'new_item_name'     => 'New Category Name',
+    'menu_name'         => 'Categories'
   ); 
   $args = array(
     'labels'            => $labels,
     'public'            => true,
-    'show_in_nav_menus' => false,
+    'show_in_nav_menus' => true,
     'show_ui'           => true,
     'show_tagcloud'     => false,
     'hierarchical'      => true,
-    'rewrite'           => false
+    'rewrite'           => array('slug' => 'category'),
+    'query_var'         => true
   );
-  register_taxonomy('faq-category', 'faq', $args);
+  register_taxonomy('blog-cat', 'blog', $args);
 }
 ?>
