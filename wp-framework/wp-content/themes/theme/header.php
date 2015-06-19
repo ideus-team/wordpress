43d40ad1c4b1d6834dd52f8191e94bf1eda47d0d
Valery Bogdanov <valery.bogdanov@ideus.biz>
2015-06-19 21:01:49 +0300

    html-framework → wp-framework

--- wp-framework/wp-content/themes/theme/header.php
+++ wp-framework/wp-content/themes/theme/header.php
@@ -1,14 +1,17 @@
 <?php
 switch (nc_device()) {
   case 'mobile':
-    $viewport = '1024';
+    $viewport     = '1024px';
+    $viewportMeta = '1024';
     break;
   case 'tablet':
-    $viewport = '1024';
+    $viewport     = 'device-width';
+    $viewportMeta = 'device-width';
     break;
   case 'desktop':
   default:
-    $viewport = 'device-width';
+    $viewport     = 'device-width';
+    $viewportMeta = 'device-width';
     break;
 }
 ?>
@@ -20,14 +23,17 @@ switch (nc_device()) {
 
   <title><?php wp_title('::', true, 'right'); ?><?php bloginfo('name'); ?></title>
 
-  <meta name="viewport" content="width=<?php echo $viewport; ?>" />
-  <?php if((nc_device() == 'mobile') && nc_ie()): ?>
-    <style>
-      @-ms-viewport {
-        width: <?php echo $viewport; ?>px;
-      }
-    </style>
-  <?php endif; ?>
+  <!-- <meta property="og:image" content="<?php echo $BASE_URL; ?>/img/userfiles/og-image.png" /> -->
+
+  <meta name="viewport" content="width=<?php echo $viewportMeta; ?>" />
+  <style>
+    @-ms-viewport {
+      width: <?php echo $viewport; ?>;
+    }
+    @viewport {
+      width: <?php echo $viewport; ?>;
+    }
+  </style>
 
   <link rel="shortcut icon" href="<?php echo site_url('/favicon.ico'); ?>" />
   <link rel="apple-touch-icon" href="<?php echo site_url('/apple-touch-icon.png'); ?>" />
@@ -46,41 +52,43 @@ switch (nc_device()) {
   <![endif]-->
   <div class="l-wrapper">
     <header class="l-siteHeader" role="banner">
-      <div class="l-siteLogo">
-        <?php
-          $siteLogo__iconURL = get_template_directory_uri().'/img/blocks/l-siteLogo/l-siteLogo-logo.png';
-          //$siteLogo__iconURL = (nc_device()=='mobile') ? get_template_directory_uri().'/img/blocks/l-siteLogo/l-siteLogo-logo-mobile.png' : get_template_directory_uri().'/img/blocks/l-siteLogo/l-siteLogo-logo.png';
-        ?>
-        <?php if(is_front_page()): ?>
-          <h1 class="b-siteLogo" itemscope itemtype="http://schema.org/Organization">
-            <a class="b-siteLogo__link" itemprop="url">
-              <img class="b-siteLogo__icon" src="<?php echo $siteLogo__iconURL; ?>" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>" itemprop="logo" />
-            </a>
-          </h1>
-        <?php else: ?>
-          <div class="b-siteLogo" itemscope itemtype="http://schema.org/Organization">
-            <a class="b-siteLogo__link" href="<?php echo site_url('/'); ?>" itemprop="url">
-              <img class="b-siteLogo__icon" src="<?php echo $siteLogo__iconURL; ?>" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>" itemprop="logo" />
-            </a>
-          </div>
-        <?php endif; ?>
+      <div class="b-siteHeader">
+        <div class="l-siteLogo">
+          <?php
+            $siteLogo__iconURL = get_template_directory_uri().'/img/blocks/l-siteLogo/l-siteLogo-logo.png';
+            //$siteLogo__iconURL = (nc_device()=='mobile') ? get_template_directory_uri().'/img/blocks/l-siteLogo/l-siteLogo-logo-mobile.png' : get_template_directory_uri().'/img/blocks/l-siteLogo/l-siteLogo-logo.png';
+          ?>
+          <?php if(is_front_page()): ?>
+            <h1 class="b-siteLogo" itemscope itemtype="http://schema.org/Organization">
+              <a class="b-siteLogo__link" itemprop="url">
+                <img class="b-siteLogo__icon" src="<?php echo $siteLogo__iconURL; ?>" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>" itemprop="logo" />
+              </a>
+            </h1>
+          <?php else: ?>
+            <div class="b-siteLogo" itemscope itemtype="http://schema.org/Organization">
+              <a class="b-siteLogo__link" href="<?php echo site_url('/'); ?>" itemprop="url">
+                <img class="b-siteLogo__icon" src="<?php echo $siteLogo__iconURL; ?>" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>" itemprop="logo" />
+              </a>
+            </div>
+          <?php endif; ?>
+        </div>
+
+        <?php get_search_form(); ?>
+
+        <nav class="l-siteNavigation" role="navigation">
+          <?php
+          wp_nav_menu(array(
+            'theme_location'  => 'header',
+            'container'       => false,
+            'menu_class'      => 'b-mainNavigation',
+            'fallback_cb'     => false,
+            'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
+            'depth'           => 1,
+            'walker'          => new nc_Walker_Nav_Menu
+          ));
+          ?>
+        </nav>
       </div>
-
-      <?php get_search_form(); ?>
-
-      <nav class="l-siteNavigation" role="navigation">
-        <?php
-        wp_nav_menu(array(
-          'theme_location'  => 'header',
-          'container'       => false,
-          'menu_class'      => 'b-mainNavigation',
-          'fallback_cb'     => false,
-          'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
-          'depth'           => 1,
-          'walker'          => new nc_Walker_Nav_Menu
-        ));
-        ?>
-      </nav>
     </header>
 
     <div class="l-content">
