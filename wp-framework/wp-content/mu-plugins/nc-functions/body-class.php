c984fdfd873a233e535eabc739ccd486069b9f7e
Valery Bogdanov <radkiller@yandex.ru>
2015-07-06 17:55:07 +0300

    Fix nc_body_class

--- wp-framework/wp-content/mu-plugins/nc-functions/body-class.php
+++ wp-framework/wp-content/mu-plugins/nc-functions/body-class.php
@@ -4,7 +4,8 @@ add_filter('body_class', 'nc_body_class');
 function nc_body_class($classes) {
   global $post;
   $post_data = get_post($post->ID, ARRAY_A);
-  $slug = $post_data['post_name'];
+
+  $slug = (is_front_page()) ? '' : $post_data['post_name'];
 
   $classes[] = (is_front_page()) ? '-page_home' : '-page_inner';
   if ($slug) {
