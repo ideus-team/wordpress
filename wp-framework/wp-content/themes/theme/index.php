9dad92fc556e7260cf44230af43534c4fc287a63
Valery Bogdanov <valery.bogdanov@ideus.biz>
2015-09-17 03:59:11 +0300

    Small fix

--- wp-framework/wp-content/themes/theme/index.php
+++ wp-framework/wp-content/themes/theme/index.php
@@ -10,7 +10,7 @@
     );
     */
     ?>
-    <section class="b-contentText b-text">
+    <section class="b-contentText">
       <h2 class="b-contentText__title"><?php the_title(); ?></h2>
       <div class="b-contentText__content b-text"><?php the_content(); ?></div>
     </section>
