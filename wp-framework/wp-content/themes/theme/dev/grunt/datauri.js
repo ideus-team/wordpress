5b55cb7aff18b0ed8aafd7c5af4f0b56edf2f34d
Valery Bogdanov <valery.bogdanov@ideus.biz>
2015-11-23 16:50:21 +0200

    Sync with html-framework

---
+++ wp-framework/wp-content/themes/theme/dev/grunt/datauri.js
@@ -0,0 +1,12 @@
+module.exports = {
+  main: {
+    options: {
+      classPrefix: 'data-',
+      checkFilesize: true,
+      usePlaceholder: true,
+      variables: false,
+    },
+    src: ['<%= sourceBase64Files %>'],
+    dest: ['<%= sourceCSSBase64 %>'],
+  },
+};
