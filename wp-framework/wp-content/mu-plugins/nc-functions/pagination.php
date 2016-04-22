7fa11db4af1866e71f10a2044bf2f5e0d3612e80
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-04-22 17:52:52 +0300

    Рефакторинг nc_pagenavi

--- wp-framework/wp-content/mu-plugins/nc-functions/pagination.php
+++ wp-framework/wp-content/mu-plugins/nc-functions/pagination.php
@@ -3,30 +3,35 @@
 function nc_pagenavi($args = array()) {
   global $wp_query, $wp_rewrite;
 
-  $args = wp_parse_args(
-    $args, array(
-      'query' => $wp_query,
-      'echo'  => true,
-    )
-  );
+  $args = wp_parse_args($args, array(
+    'query'     => $wp_query,
+    'mid_size'  => 1,
+    'end_size'  => 3,
+    'prev_text' => '←',
+    'next_text' => '→',
+    'type'      => 'array',
+    'class'     => 'b-pagination',
+    'modifier'  => '',
+    'found'     => true, // Show "Found N"
+    'total'     => true, // Show "Page N of N"
+    'echo'      => true,
+  ));
 
   $query = $args['query'];
 
   $pages = '';
   $max = $query->max_num_pages;
   if (!$current = get_query_var('paged')) $current = 1;
-  $a['base'] = str_replace(999999999, '%#%', get_pagenum_link(999999999));
-  $a['total'] = $max;
-  $a['current'] = $current;
-
-  $found = true; // Show "Found N"
-  $total = true; // Show "Page N of N"
-  $a['mid_size'] = 1;
-  $a['end_size'] = 3;
-  $a['prev_text'] = '←';
-  $a['next_text'] = '→';
-  $a['type'] = 'array';
-  $class = 'b-pagination';
+  $paginate = array(
+    'base'      => str_replace(999999999, '%#%', get_pagenum_link(999999999)),
+    'total'     => $max,
+    'current'   => $current,
+    'mid_size'  => $args['mid_size'],
+    'end_size'  => $args['end_size'],
+    'prev_text' => $args['prev_text'],
+    'next_text' => $args['next_text'],
+    'type'      => $args['type'],
+  );
 
   $search = array(
     'page-numbers',
@@ -37,7 +42,7 @@ function nc_pagenavi($args = array()) {
     '/page/1/'
   );
   $replace = array(
-    $class.'__link',
+    $args['class'].'__link',
     '-type_prev',
     '-type_next',
     '-type_dots',
@@ -46,15 +51,19 @@ function nc_pagenavi($args = array()) {
   );
 
   if ($max > 1) {
-    $pages .= '<div class="'.$class.'">'."\r";
+    $pages .= '<div class="'.$args['class'].($args['modifier'] ? ' '.$args['modifier'] : '').'">'."\r";
 
-    if ($found) $pages .= '<span class="'.$class.'__total">Found '.$query->found_posts.'</span>'."\r";
-    if ($total) $pages .= '<span class="'.$class.'__pages">Page '.$current.' of '.$max.'</span>'."\r";
+    if ($args['found']) {
+      $pages .= '<span class="'.$args['class'].'__total">Found '.$query->found_posts.'</span>'."\r";
+    }
+    if ($args['total']) {
+      $pages .= '<span class="'.$args['class'].'__pages">Page '.$current.' of '.$max.'</span>'."\r";
+    }
 
-    $pages .= '<ul class="'.$class.'__list">'."\r";
-    $paginationList = str_replace($search, $replace, paginate_links($a));
+    $pages .= '<ul class="'.$args['class'].'__list">'."\r";
+    $paginationList = str_replace($search, $replace, paginate_links($paginate));
     foreach ($paginationList as $value) {
-      $pages .= '<li class="'.$class.'__item">'.$value.'</li>'."\r";
+      $pages .= '<li class="'.$args['class'].'__item">'.$value.'</li>'."\r";
     }
     $pages .= '</ul>'."\r";
 
@@ -75,6 +84,9 @@ function nc_pageNav($args = array()) {
   $args = wp_parse_args(
     $args, array(
       'query' => $wp_query,
+      'class' => 'b-pageNav',
+      'next'  => 'Older Entries',
+      'prev'  => 'Newer Entrie',
       'echo'  => true,
     )
   );
@@ -84,20 +96,16 @@ function nc_pageNav($args = array()) {
   $pages = '';
   $max = $query->max_num_pages;
 
-  $class = 'b-pageNav';
-  $next = 'Older Entries';
-  $prev = 'Newer Entrie';
-
   if ($max > 1) {
-    $pages .= '<div class="'.$class.'">'."\r";
+    $pages .= '<div class="'.$args['class'].'">'."\r";
 
-    $pages .= '<ul class="'.$class.'__list">'."\r";
+    $pages .= '<ul class="'.$args['class'].'__list">'."\r";
 
-    $nextLink = get_next_posts_link($next, $max);
-    $prevLink = get_previous_posts_link($prev);
+    $nextLink = get_next_posts_link($args['next'], $max);
+    $prevLink = get_previous_posts_link($args['prev']);
 
-    if ($nextLink) $pages .= '<li class="'.$class.'__item -type_old">'.$nextLink.'</li>'."\r";
-    if ($prevLink) $pages .= '<li class="'.$class.'__item -type_new">'.$prevLink.'</li>'."\r";
+    if ($nextLink) $pages .= '<li class="'.$args['class'].'__item -type_old">'.$nextLink.'</li>'."\r";
+    if ($prevLink) $pages .= '<li class="'.$args['class'].'__item -type_new">'.$prevLink.'</li>'."\r";
 
     $pages .= '</ul>'."\r";
 
