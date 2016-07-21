01eb8fa00fc9dfd7ac5063d97dbc30ef6eeb70d5
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-07-21 19:40:30 +0300

    Redirect To First Child template

---
+++ snippets/redirectToFirstChild.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Redirect To First Child
+*/
+while (have_posts()) {
+  the_post();
+
+  $pages = get_pages( array(
+    'sort_order'   => 'ASC',
+    'sort_column'  => 'menu_order',
+    'parent'       => get_the_ID(),
+    'number'       => 1,
+  ) );
+
+  $first = $pages[0];
+  wp_redirect(get_permalink($first->ID));
+}
+?>
