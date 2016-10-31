859545e310e647a29223539b374fa4b9773741a9
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-10-31 12:57:27 +0200

    edit nc_robotstxt

--- snippets/nc_robotstxt.php
+++ snippets/nc_robotstxt.php
@@ -2,7 +2,7 @@
 // Change robots.txt
 add_filter('robots_txt', 'nc_robotstxt');
 function nc_robotstxt($text){
-  $text .= 'Allow: /sitemap/';
+  $text .= 'Allow: /sitemap/'."\n";
   return $text;
 }
 ?>
