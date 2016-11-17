4bf0815c50e9a3028fb64d747f5b78f449192cb9
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-11-18 00:21:31 +0200

    small fix

--- functions/slug.php
+++ functions/slug.php
@@ -1,9 +1,9 @@
 <?php
-//Получение slug поста по его ID
+//РџРѕР»СѓС‡РµРЅРёРµ slug РїРѕСЃС‚Р° РїРѕ РµРіРѕ ID
 function get_slug($id) {
   $post_data = get_post($id, ARRAY_A);
   $slug = $post_data['post_name'];
-  return $slug; 
+  return $slug;
 }
 
 function the_slug($id) {
