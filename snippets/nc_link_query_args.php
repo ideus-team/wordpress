ec949b9981e15b8bc38165b6586201c5f53600b1
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-08-17 14:28:46 +0300

    nc_link_query_args

---
+++ snippets/nc_link_query_args.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Hide some post types from the "link to existing content"
+ */
+add_filter( 'wp_link_query_args', 'nc_link_query_args' );
+function nc_link_query_args ($query ) {
+  $pt_new = array();
+  $exclude_types = array( 'dictionary' );
+
+  foreach ( $query['post_type'] as $pt ) {
+    if ( in_array( $pt, $exclude_types ) ) continue;
+    $pt_new[] = $pt;
+  }
+
+  $query['post_type'] = $pt_new;
+
+  return $query;
+}
