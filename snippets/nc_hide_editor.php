afbee406f0fa081fe8f82bf866a231127e60d9a8
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-12-05 21:56:24 +0200

    Hide editor on specific page

---
+++ snippets/nc_hide_editor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Hide editor on specific page
+ */
+add_action( 'user_can_richedit', 'nc_hide_editor' );
+function nc_hide_editor() {
+  global $post;
+
+  $templates = array(
+    'page-templates/home.php',
+    'page-templates/template.php',
+  );
+  $template_file = get_post_meta( $post->ID, '_wp_page_template', true );
+
+  if ( in_array( $template_file, $templates ) ) {
+    remove_post_type_support( 'page', 'editor' );
+  }
+}
