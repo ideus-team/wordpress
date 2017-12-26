6ff01476c7f86d8cb1415e95c2201e9542c94008
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-12-26 23:13:51 +0200

    fix nc_hide_editor

--- snippets/nc_hide_editor.php
+++ snippets/nc_hide_editor.php
@@ -6,14 +6,16 @@ add_action( 'user_can_richedit', 'nc_hide_editor' );
 function nc_hide_editor() {
   global $post;
 
-  $templates = array(
-    'page-templates/home.php',
-    'page-templates/template.php',
-  );
-  $template_file = get_post_meta( $post->ID, '_wp_page_template', true );
+  if ( $post ) {
+    $templates = array(
+      'page-templates/home.php',
+      'page-templates/template.php',
+    );
+    $template_file = get_post_meta( $post->ID, '_wp_page_template', true );
 
-  if ( in_array( $template_file, $templates ) ) {
-    remove_post_type_support( 'page', 'editor' );
+    if ( in_array( $template_file, $templates ) ) {
+      remove_post_type_support( 'page', 'editor' );
+    }
   }
 
   return true;
