4d3cb16409f309fc2c8d9309682e1c6b15747fcd
Valery Bogdanov <valery.bogdanov@ideus.biz>
2018-05-17 18:34:14 +0300

    small fix

--- snippets/langSwitch.php
+++ snippets/langSwitch.php
@@ -14,7 +14,7 @@
   <?php if ( $nc_langSwitch ) : ?>
     <ul class="b-langSwitch">
       <?php foreach ( $nc_langSwitch as $item ) : ?>
-        <li class="b-langSwitch__item -lang_<?php echo $item['slug']; ?><?php if ( $item['current_lang'] ): ?> -state_current<?php endif; ?><?php if ( $item['no_translation'] ): ?> -translation_false<?php endif; ?>" style="background-image: url('<?php echo $item['flag']; ?>');">
+        <li class="b-langSwitch__item -lang_<?php echo $item['slug']; ?><?php if ( $item['current_lang'] ) : ?> -state_current<?php endif; ?><?php if ( $item['no_translation'] ) : ?> -translation_false<?php endif; ?>" style="background-image: url('<?php echo $item['flag']; ?>');">
           <a class="b-langSwitch__link" href="<?php echo $item['url']; ?>"><?php echo $item['name']; ?></a>
         </li>
       <?php endforeach; ?>
