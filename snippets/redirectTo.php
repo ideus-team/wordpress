1448145cdb66a5431e1b1e86127e3f52aead8d75
Valerii Bohdanov <valery.bogdanov@ideus.biz>
2019-02-18 19:39:25 +0200

    Template Name: Redirect to other post

---
+++ snippets/redirectTo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template Name: Redirect to other post
+ * Template Post Type: page
+ */
+if ( get_field( '_nc_redirect' ) ) {
+  wp_redirect( get_permalink( get_field( '_nc_redirect' ) ) );
+  exit;
+}
