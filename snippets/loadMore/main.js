c653eaf25921c1d801dd3c7f6ff7c205d9df88d8
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2021-07-05 16:23:19 +0300

    Fixed loadMore()

--- snippets/loadMore/main.js
+++ snippets/loadMore/main.js
@@ -7,6 +7,12 @@ function loadMore() {
     var button = $(this);
     var container = $( button.data('container') );
 
+    if ( button.hasClass('-disabled_true') ) {
+      return false;
+    }
+
+    button.addClass('-disabled_true');
+
     var postdata = {
       'post_type' : button.data('post_type'),
       'offset'    : button.data('offset'),
@@ -22,6 +28,8 @@ function loadMore() {
       },
       dataType: 'json',
       success: function(result){
+        button.removeClass('-disabled_true');
+
         if ( result.success == true ) {
           container.append( result.data.content );
 
