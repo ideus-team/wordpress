1e90f43f71af3110e06a732743979f48a0208c56
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2023-07-06 17:39:35 +0300

    Template Name: Redirect to other URL

---
+++ snippets/templates/redirect-to-url.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Redirect to other URL
+ * Template Post Type: page
+ */
+
+if ( get_field( '_nc_redirect' ) ) {
+  wp_redirect( get_field( '_nc_redirect' ) );
+  exit;
+}
