395bcef00330557fd51cf287948baf149474b134
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2025-02-13 01:08:28 +0200

    Fix WPCS

--- snippets/WP_Query.php
+++ snippets/WP_Query.php
@@ -2,16 +2,21 @@
 /**
  * WP_Query example
  */
-$query = new WP_Query( array(
-	'post_type'      => 'post',
-	'orderby'        => array( 'date' => 'DESC' ),
-	'paged'          => get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1,
-	'posts_per_page' => 5,
-) );
+$query = new WP_Query(
+	array(
+		'post_type'      => 'post',
+		'orderby'        => array( 'date' => 'DESC' ),
+		'paged'          => get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1,
+		'posts_per_page' => 5,
+	)
+);
 if ( $query->have_posts() ) :
 ?>
 
-	<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+	<?php
+	while ( $query->have_posts() ) :
+		$query->the_post();
+		?>
 
 	<?php endwhile; ?>
 
